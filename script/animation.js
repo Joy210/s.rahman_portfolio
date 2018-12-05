8828545c45a455be8fc287247494459d45c39153
joy210 <md.sohanur.rahman.joy@gmail.com>
2018-12-04 21:14:47 -0800

    update portfolio

--- script/animation.js
+++ script/animation.js
@@ -2,38 +2,20 @@
 window.sr = ScrollReveal();
 
 sr.reveal('.img', {
-    duration: 2000,
+    duration: 1000,
     origin: 'bottom',
-    distance: '100px',
-    delay: 1000
+    distance: '150px',
+    delay: 750
 });
-sr.reveal('p', {
-    duration: 2000,
+sr.reveal('.box', {
+    duration: 1000,
     origin: 'left',
     distance: '100px',
     delay: 500
 });
 sr.reveal('.btn', {
-    duration: 2000,
+    duration: 1000,
     origin: 'bottom',
     distance: '20px',
-    delay: 1000
-});
-sr.reveal('.box', {
-    duration: 2000,
-    origin: 'bottom',
-    distance: '100px',
-    delay: 1000
-});
-sr.reveal('.fas', {
-    duration: 2000,
-    origin: 'top',
-    distance: '50px',
-    delay: 1000
-});
-sr.reveal('.far', {
-    duration: 2000,
-    origin: 'top',
-    distance: '50px',
-    delay: 1000
+    delay: 50
 });
